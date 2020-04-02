0d61a62f0b2eac15fd73e949caa7239cdb246548
tomiko2039 <60902352+tomiko2039@users.noreply.github.com>
2020-04-02 09:53:50 -0600

    Add files via upload

--- js-minecraft-fishing-sim-start-master/main.js
+++ js-minecraft-fishing-sim-start-master/main.js
@@ -3,8 +3,13 @@
 
 // Global Variables
 let numFish = 0;
+let numSalmon = 0;
+let numPuffer = 0;
+let numClown = 0;
 let numJunk = 0;
 let numTresure = 0;
+
+let resultsEl = document.getElementById('results');
 
 // Event Listener
 document.getElementById('mainImg').addEventListener('click', catchFish);
@@ -16,13 +21,40 @@ function catchFish() {
 
     // Simulate results
     if (randNum < 0.85) {
-        numFish++;
-        document.getElementById('fish').innerHTML = numFish;
-    }else if (randNum < 0.95) {
+        // Catch Fish - Simulate what type of fish to catch
+        let randFish = Math.random();
+
+        if (randFish < 0.5) {
+            console.log("RawFish")
+            numFish++;
+            document.getElementById('fish').innerHTML = numFish;
+            resultsEl.innerHTML += '<img src="images/RawFish.png">'
+        } else if (randFish < 0.7) {
+            console.log("RawSalmon")
+            numSalmon++;
+            document.getElementById('salmon').innerHTML = numSalmon;
+            resultsEl.innerHTML += '<img src="images/Raw_Salmon.png">'
+        } else if (randFish < 0.9) {
+            console.log("PufferFish")
+            numPuffer++;
+            document.getElementById('puffer').innerHTML = numPuffer;
+            resultsEl.innerHTML += '<img src="images/Pufferfish_old.png">'
+        } else {
+            console.log("ClownFish")
+            numClown++;
+            document.getElementById('clown').innerHTML = numFish;
+            resultsEl.innerHTML += '<img src="images/Clownfish.png">'
+        }
+
+    } else if (randNum < 0.95) {
+        // Catch Junk
         numJunk++;
         document.getElementById('junk').innerHTML = numJunk;
-    }else {
+        resultsEl.innerHTML += '<img src="images/leatherboots.png">'
+    } else {
+        // Catch Treasure
         numTresure++;
         document.getElementById('treasure').innerHTML = numTresure;
+        resultsEl.innerHTML += '<img src="images/Bow.png">'
     }
 }
